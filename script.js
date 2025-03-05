648109365c84e7e64fdc60abcd5c73e23546c4d1
muthu-25 <68727855+muthu-25@users.noreply.github.com>
2025-03-05 01:12:52 +0000

    Added DOM JS to display in webpage
    Added the required script to make the game work in the webpage itself.

--- script.js
+++ script.js
@@ -1,39 +1,62 @@
+let rockBtn = document.getElementById("Rock");
+let paperBtn = document.getElementById("Paper");
+let scissorsBtn = document.getElementById("Scissors");
+let paraRes = document.getElementById("respara");
+
+rockBtn.addEventListener('click', () => {
+    let computerChoice = getComputerChoice();
+    playGame('rock', computerChoice);
+});
+
+paperBtn.addEventListener('click', () => {
+    let thecomputerChoice = getComputerChoice();
+    playGame('paper', thecomputerChoice);
+});
+
+scissorsBtn.addEventListener('click', () => {
+    let computerChoice = getComputerChoice();
+    playGame('scissors', computerChoice);
+});
+
+
+
 function getComputerChoice(){
     let results = ["Rock", "Paper", "Scissors"];
     return results[Math.floor(Math.random() * results.length)]
 }
 
-function getHumanChoice(){
-    let choice = prompt("Rock, Paper or Scissors?", "Rock");
-    return choice;
-}
+let humanScore = 0;
+let computerScore = 0;
 
-function playGame(){
-    let humanScore = 0;
-    let computerScore = 0;
+
+function playGame(humanChoice, computerChoice){
     
-    function playRound(humanChoice, computerChoice){
-        humanChoice = humanChoice.toLowerCase();
-        computerChoice = computerChoice.toLowerCase();
-        let finalResult = "invalid";
-        if(humanChoice === computerChoice){
-            finalResult = "draw";
-        } else if (humanChoice == "rock" && computerChoice == "paper" ||humanChoice == "paper" && computerChoice == "scissors" || humanChoice == "scissors" && computerChoice == "rock" ){
-            finalResult = `You Lose! ${computerChoice} beats ${humanChoice} `;
-            computerScore += 1;
-        } else if (computerChoice == "rock" && humanChoice == "paper" ||computerChoice == "paper" && humanChoice == "scissors" || computerChoice == "scissors" && humanChoice == "rock" ){
-            finalResult = `You Win! ${humanChoice} beats ${computerChoice} `;
-            humanScore += 1
-        } 
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+    let finalResult = "invalid";
+    if(humanChoice === computerChoice){
+        finalResult = "draw";
+    } else if (humanChoice == "rock" && computerChoice == "paper" ||humanChoice == "paper" && computerChoice == "scissors" || humanChoice == "scissors" && computerChoice == "rock" ){
+        finalResult = `You Lose! <br> ${computerChoice} beats ${humanChoice} `;
+        computerScore += 1;
+    } else if (computerChoice == "rock" && humanChoice == "paper" ||computerChoice == "paper" && humanChoice == "scissors" || computerChoice == "scissors" && humanChoice == "rock" ){
+        finalResult = `You Win! <br> ${humanChoice} beats ${computerChoice} `;
+        humanScore += 1
+    } 
 
-        console.log(finalResult);
-        console.log("Human Score:" + humanScore);
-        console.log("Computer Score:" + computerScore);
+    paraRes.innerHTML = "";
+    paraRes.innerHTML += "<br>" + finalResult;
+    paraRes.innerHTML += "<br>" + " Human Score:" + humanScore;
+    paraRes.innerHTML += "<br>" + " Computer Score:" + computerScore;
+    paraRes.innerHTML += "<br>" 
+
+    if (humanScore >= 5 ){
+        paraRes.innerHTML += "<br>" + "You Win!!!";
+        humanScore = 0;
+        computerScore = 0;
+    } else if (computerScore >= 5){
+        paraRes.innerHTML += "<br>" + "You lost the game, good luck next time!";
+        humanScore = 0;
+        computerScore = 0;
     }
-
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
 }
